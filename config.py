0d79ac7b88ea9b80dc644f7312cec7a374d666f5
Adama Faye <46406662+adamafaye1945@users.noreply.github.com>
2022-06-11 17:24:59 -0400

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-
-
-class MY_KEY():
-    def __init__(self):
-        self.open_weather_api_key = "a0bb1515de9f37ce1f0384cd500d9753"
-        self.twilio_api_SID ="AC83798e08edf7e70f0075bc7b9ca606e7"
-        self.twilio_auth_token= "0959d5a18a8439a626574f75684addd6"
-
